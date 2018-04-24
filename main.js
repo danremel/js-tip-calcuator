02444331b9c55e2fee6f62137221204813608eba
danremel <remel.dan@gmail.com>
2018-04-24 12:16:19 -0400

    Created beggining of calculator function.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-document.write("hello world");
+var calculateTip = function() {
+    // Linking variables with form values
+    var billAmount = document.getElementById("bill-amount").value;
+    var service = document.getElementById("select-service").value;
+    var peopleCount = document.getElementById("people-count").value;
+
+    // Calculate the tip amount, then divide by number of people.
+    var tipTotal = (billAmount * service) / peopleCount;
+}
